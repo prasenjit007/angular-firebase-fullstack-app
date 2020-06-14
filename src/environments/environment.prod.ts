7c89124dc30f83d0e48db3a2715cff16268afc9e
prasenjit_samanta <prasenjit.samanta007@gmail.com>
2020-06-14 18:07:11 +0530

    Dummy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDcQ_aoqkSOm1D_k7fhBqNAXiW2pJtRn3M",
-    authDomain: "ng-fitness-tracker-007.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-007.firebaseio.com",
+    apiKey: "tRn3M",
+    authDomain: "ng-fpp.com",
+    databaseURL: "httpio.com",
     projectId: "ng-fitness-tracker-007",
-    storageBucket: "ng-fitness-tracker-007.appspot.com",
-    messagingSenderId: "666655825408",
-    appId: "1:666655825408:web:b40b3f7f9424b8215e02e5",
-    measurementId: "G-DGTDD93440"
+    storageBucket: "ng-fit.com",
+    messagingSenderId: "8",
+    appId: "1:02e5",
+    measurementId: "0"
   }
 };
