896875503d8b0a94602ffb10082438537e0b46cd
prasenjit_samanta <prasenjit.samanta007@gmail.com>
2020-06-14 18:14:38 +0530

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "tRn3M",
-    authDomain: "ng-fpp.com",
-    databaseURL: "httpio.com",
-    projectId: "ng-fitness-tracker-007",
-    storageBucket: "ng-fit.com",
-    messagingSenderId: "8",
-    appId: "1:02e5",
-    measurementId: "0"
+    apiKey: "test",
+    authDomain: "test",
+    databaseURL: "test",
+    projectId: "test",
+    storageBucket: "test",
+    messagingSenderId: "test",
+    appId: "test",
+    measurementId: "test"
   }
 };
