517785c495f844091ccb4d59fbc7f4f22fa7d0ea
prasenjit_samanta <prasenjit.samanta007@gmail.com>
2020-06-14 18:06:04 +0530

    Update environment.ts
    Dummy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: true,
 
   firebase: {
-    apiKey: "AIzaSyDcQ_aoqkSOm1D_k7fhBqNAXiW2pJtRn3M",
-    authDomain: "ng-fitness-tracker-007.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-007.firebaseio.com",
-    projectId: "ng-fitness-tracker-007",
-    storageBucket: "ng-fitness-tracker-007.appspot.com",
-    messagingSenderId: "666655825408",
-    appId: "1:666655825408:web:b40b3f7f9424b8215e02e5",
-    measurementId: "G-DGTDD93440"
+    apiKey: "AIzaSJtRn3M",
+    authDomain: "nirebaseapp.com",
+    databaseURL: "htt7.firebaseio.com",
+    projectId: "ng-fitne007",
+    storageBucket: "ng-fitnpspot.com",
+    messagingSenderId: "408",
+    appId: "1:666215e02e5",
+    measurementId: "40"
   }
 };
 
