0230afea43ac64412c29543f2e25b7277c97ad88
prasenjit_samanta <prasenjit.samanta007@gmail.com>
2023-07-13 22:38:34 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "test",
-    authDomain: "test",
-    databaseURL: "test",
-    projectId: "test",
-    storageBucket: "test",
-    messagingSenderId: "test",
-    appId: "test",
-    measurementId: "test"
-  }
-};
+
